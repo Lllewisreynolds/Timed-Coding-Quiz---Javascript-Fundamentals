c13debdce027c0b02203bc4099ad6fb597ce4405
Lllewisreynolds <lsreynolds108@gmail.com>
2023-10-25 16:09:19 +0100

    Shell of Countdown Timer function displayed at top-right of web page created

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,12 @@
+// Scope is important! Scope is important!!!!!
+
+// Don't sweat order of sub-function creation - compartmentalise my code and then move things around after / figure out when to call it later?
+
+
+// Global scope variables 
+
+// HTML elements I will need to pull through and manipulate in JavaScript to provide additional quiz functionality
+
 var highScoresEl = document.getElementById("highScores");
 var currentScoreEl = document.getElementById("currentScore");
 var timerEl = document.getElementById('timer');
@@ -11,6 +20,28 @@ var questionFiveEl = document.getElementById('questionFive');
 var quizResultEl = document.getElementById('quizResult');
 var finalScoreEl = document.getElementById('finalScore');
 var submitScoreEl = document.getElementById('submitScore');
+
+// Time allocated at start of quiz - will be manipulated when questions answered incorrectly
+var countdownTimer = 90;
+// Boolean declared in global scope as false. Once state is switched by triggering of 'Start Quiz' button master function, countdown will display and decrement
+var quizTimerStart = false;
+
+// Function declared to manipulate countdown as quiz duration begins
+function countdownTimerStart() {
+// Local variable declared within function to set interval decrement 
+    var countdownTimerDecrement = setInterval(function() {
+        !quizTimerStart;
+        countdownTimer --;
+        timerEl.textContent = countdownTimer;
+        
+    if (countdownTimer === 0) {
+        clearInterval(countdownTimerDecrement);
+        // as of yet undefined function
+        endquiz();
+    }
+}, 1000);
+}
+
 
 
 // GIVEN I am taking a code quiz
